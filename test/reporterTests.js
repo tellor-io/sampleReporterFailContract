4d9cc939f87eab6bf831c4552aae82a7f8f026ae
brendaloya <l.loya.b@gmail.com>
2022-04-26 12:10:42 -0400

    checking owner on transfer function

--- test/reporterTests.js
+++ test/reporterTests.js
@@ -113,6 +113,12 @@ describe("Reporter Tests", function() {
       await reporter.transfer(accounts[0].address, 200);
       assert(await master.balanceOf(accounts[0].address) == 200, "transfer should be successful")
     });
+    it("transfer() fail if msg.sender is not owner", async function() {
+      console.log("initial bal", await master.balanceOf(accounts[5].address)*1)
+      h.expectThrow(await reporter.connect(accounts[5]).transfer(accounts[5].address, 200));
+      console.log(await master.balanceOf(accounts[5].address)*1)
+      assert(await master.balanceOf(accounts[5].address) == 0, "transfer was not successful")
+    });
     it("withdrawStake()", async function() {
         await reporter.depositStake();
         await reporter.requestStakingWithdraw();
